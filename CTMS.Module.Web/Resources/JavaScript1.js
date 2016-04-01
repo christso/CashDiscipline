87a1123a82e718cdcece711a91606405153d4566
Christopher Tso <christso@gmail.com>
2016-04-01 15:37:26 +1100

    deleted Web project

--- CTMS.Module.Web/Resources/JavaScript1.js
+++
@@ -1,8 +0,0 @@
-﻿// test only: delete if not used
-function MinimizeRP(panelId, separatorImageId, collapseLeft) {
-    var LPcell = document.getElementById(panelId);
-    LPcell.style.display = 'none';
-    UpdateSeparatorsImages(separatorImageId, collapseLeft, LPcell.style.display);
-    _aspxSetCookie(panelId + postfix, LPcell.style.display);
-    AdjustSize();
-}
