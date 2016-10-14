564131ccc2d90d676c0e2f16aa16577c1e84874c
Christopher Tso <christso@gmail.com>
2016-10-15 09:55:57 +1100

    add launcher

---
+++ CashDiscipline.Launcher/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashDiscipline.Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directoryName = System.IO.Path.GetDirectoryName(location);
+            string programPath = System.IO.Path.Combine(directoryName, "Cash Discipline.bat");
+            System.Diagnostics.Process.Start(programPath);
+        }
+    }
+}
