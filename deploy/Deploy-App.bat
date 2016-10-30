f39df0ecc58517bdac97701979a12cc4ee03d8d4
Christopher Tso <christso@gmail.com>
2016-10-31 09:45:05 +1100

    update paths

--- deploy/Deploy-App.bat
+++ deploy/Deploy-App.bat
@@ -1,2 +1,2 @@
-robocopy "E:\Models\Cash Discipline\CashDiscipline.Win\bin\Release" "\\vhacorp07.vha.internal\vodashare$\Finance\Cash Discipline\Apps\Cash Discipline Win" /MIR
-robocopy "E:\Models\Cash Discipline\deploy\standing" "\\vhacorp07.vha.internal\vodashare$\Finance\Cash Discipline\Apps\Cash Discipline Win"
+robocopy "D:\Models\Cash Discipline\CashDiscipline.Win\bin\Release" "\\vhacorp07.vha.internal\vodashare$\Finance\Cash Discipline\Apps\Cash Discipline Win" /MIR
+robocopy "D:\Models\Cash Discipline\deploy\standing" "\\vhacorp07.vha.internal\vodashare$\Finance\Cash Discipline\Apps\Cash Discipline Win"
