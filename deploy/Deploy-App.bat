0f7818d355f8a32b87986ca42d320e0539b0d4db
Christopher Tso <christso@gmail.com>
2016-11-06 22:18:37 +1100

    update deployment

--- deploy/Deploy-App.bat
+++ deploy/Deploy-App.bat
@@ -1,3 +1,3 @@
-robocopy "D:\Models\Cash Discipline\CashDiscipline.Win\bin\Release" "\\vhacorp07.vha.internal\vodashare$\Finance\Cash Discipline\Apps\Cash Discipline Win" /MIR
-robocopy "D:\Models\Cash Discipline\deploy\standing" "\\vhacorp07.vha.internal\vodashare$\Finance\Cash Discipline\Apps\Cash Discipline Win"
+robocopy "D:\Models\Cash Discipline\CashDiscipline.Win\bin\Release" "\\vvsw31414\share\Cash Discipline Win" /MIR
+robocopy "D:\Models\Cash Discipline\deploy\standing" "\\vvsw31414\share\Cash Discipline Win"
 pause
