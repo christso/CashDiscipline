93c1b3f888d0b50c13a3f91d9bfdef2bb6ef77de
Christopher Tso <christso@gmail.com>
2016-11-06 16:52:59 +1100

    minor changes

--- CashDiscipline.Module/Controllers/InitializeNonPersistentListViewWindowController.cs
+++ CashDiscipline.Module/Controllers/InitializeNonPersistentListViewWindowController.cs
@@ -31,13 +31,15 @@ namespace CashDiscipline.Module.Controllers
         {
             BindingList<Welcome> objects = new BindingList<Welcome>();
 
-            objects.Add(new Welcome() { Category = "Important Links", Name = "Working Capital Reports", TextValue = @"\\vhacorp07.vha.internal\vodashare$\Finance\Cash Reports\Working Capital" });
-            objects.Add(new Welcome() { Category = "Important Links", Name = "Customer Reports", TextValue = @"\\vhacorp07.vha.internal\vodashare$\Finance\Cash Reports\Customer Investment" });
-            objects.Add(new Welcome() { Category = "Important Links", Name = "Accounting Reports", TextValue = @"\\vhacorp07.vha.internal\vodashare$\Finance\Cash Reports\Accounting" });
+            objects.Add(new Welcome() { Category = "Linked Reports", Name = "Working Capital Reports", TextValue = @"\\vhacorp07.vha.internal\vodashare$\Finance\Cash Reports\Working Capital" });
+            objects.Add(new Welcome() { Category = "Linked Reports", Name = "Customer Reports", TextValue = @"\\vhacorp07.vha.internal\vodashare$\Finance\Cash Reports\Customer Investment" });
+            objects.Add(new Welcome() { Category = "Linked Reports", Name = "Accounting Reports", TextValue = @"\\vhacorp07.vha.internal\vodashare$\Finance\Cash Reports\Accounting" });
 
-            objects.Add(new Welcome() { Category = "Session Details", Name = "Username", TextValue = Environment.UserName });
+            objects.Add(new Welcome() { Category = "Session Details", Name = "User Name", TextValue = Environment.UserName });
+            objects.Add(new Welcome() { Category = "Session Details", Name = "Logon Domain", TextValue = Environment.UserDomainName });
             objects.Add(new Welcome() { Category = "Session Details", Name = "Database Connection", TextValue = Application.ConnectionString });
             objects.Add(new Welcome() { Category = "Session Details", Name = "Application Path", TextValue = System.Reflection.Assembly.GetExecutingAssembly().Location });
+            objects.Add(new Welcome() { Category = "Session Details", Name = "Application Host Name", TextValue = Environment.MachineName });
 
             e.Objects = objects;
         }
